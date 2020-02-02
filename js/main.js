909de55f9c92b6a40c929400fe8771ebe6e2020e
angryboo <buyongsong@gmail.com>
2020-02-02 23:23:52 +0900

    2020-02-02 패스트 캠퍼스 스터디 1. 제이쿼리 이용 세부효과 적용 완료 2. 접근성 검토 완료

--- js/main.js
+++ js/main.js
@@ -11,10 +11,20 @@ dropdown.on('click keyup focus', function(e){
     sublist.slideDown();
     sublist.removeClass('sublist-hide');
     sublist.addClass('sublist-act');
+    header.addClass('scroll-bottom');
 })
 dropdown.on('focusout', function(e){
     sublist.slideUp();
     sublist.addClass('sublist-hide');
     sublist.removeClass('sublist-act');
+    header.removeClass('scroll-bottom');
 })
 
+$(document).scroll(function() {
+	var scroll = $(document).scrollTop();
+	if (scroll > 0) {
+        $(header).addClass("scroll-bottom");
+	} else {
+        $(header).removeClass("scroll-bottom");
+	}
+});
