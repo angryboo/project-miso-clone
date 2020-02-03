4f8e5e6952401f54a4c31f0abda74b0f05112945
angryboo <buyongsong@gmail.com>
2020-02-03 22:17:44 +0900

    2020-02-03 패스트 캠퍼스 스터디 1. 고객센터 페이지 완성

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var header = $('header')
 var body = $('body')
 
 
-
 dropdown.on('click keyup focus', function(e){
     sublist.slideDown();
     sublist.removeClass('sublist-hide');
@@ -23,8 +22,29 @@ dropdown.on('focusout', function(e){
 $(document).scroll(function() {
 	var scroll = $(document).scrollTop();
 	if (scroll > 0) {
-        $(header).addClass("scroll-bottom");
+        header.addClass("scroll-bottom");
 	} else {
-        $(header).removeClass("scroll-bottom");
+        header.removeClass("scroll-bottom");
 	}
 });
+
+
+
+var tabs = $('.consumer__tap-list [role="tab"]');
+var tabButton = $('.consumer__tabpanel-question-button')
+var tabContent = $('.consumer__tabpanel-question-content')
+
+tabs.on('click', function(e){
+	e.preventDefault();
+	$(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
+	var selectedId = '#' + $(this).attr('aria-controls');
+	$(selectedId).addClass('section-act').siblings().removeClass('section-act');
+});
+
+
+
+tabButton.on('click', function(e){
+    e.preventDefault();
+    $(this).next().slideToggle();
+});
+
