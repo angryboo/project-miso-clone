1a256a0eac62dcf20e907c961124ba046aeffbba
angryboo <buyongsong@gmail.com>
2020-02-05 22:19:05 +0900

    2020-02-05

--- js/main.js
+++ js/main.js
@@ -3,28 +3,27 @@ var dropdown = $('.nav__drop-down');
 var sublist = $('.nav__sublist');
 var subitem = $('nav__sublist-item')
 var header = $('header')
-var body = $('body')
-
 
 dropdown.on('click keyup focus', function(e){
     sublist.slideDown();
-    sublist.removeClass('sublist-hide');
-    sublist.addClass('sublist-act');
+    // sublist.removeClass('sublist-hide');
+    // sublist.addClass('sublist-act');
     header.addClass('scroll-bottom');
 })
 dropdown.on('focusout', function(e){
     sublist.slideUp();
-    sublist.addClass('sublist-hide');
-    sublist.removeClass('sublist-act');
+    // sublist.addClass('sublist-hide');
+    // sublist.removeClass('sublist-act');
     header.removeClass('scroll-bottom');
 })
+
 
 $(document).scroll(function() {
 	var scroll = $(document).scrollTop();
 	if (scroll > 0) {
-        header.addClass("scroll-bottom");
+        header.addClass('scroll-bottom');
 	} else {
-        header.removeClass("scroll-bottom");
+        header.removeClass('scroll-bottom');
 	}
 });
 
